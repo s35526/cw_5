e3a233e2259b5c900836eb4d64ce89ccf2cd0c89
s35526 <s35526@pjwstk.edu.pl>
2026-04-21 22:40:14 +0200

    Klasa Tester

--- src/src/Tester.java
+++ src/src/Tester.java
@@ -1,2 +1,45 @@
-public class Tester {
+public class Tester extends Pracownik {
+    private boolean czyAutomatyzujacy;
+    private int liczbaScenariuszy;
+
+    public Tester(String idPracownika, String imie, String nazwisko,
+                  double stawkaBazowa, boolean czyAutomatyzujacy, int liczbaScenariuszy) {
+        super(idPracownika, imie, nazwisko, stawkaBazowa);
+        this.czyAutomatyzujacy = czyAutomatyzujacy;
+        this.liczbaScenariuszy = liczbaScenariuszy;
+    }
+
+    public boolean isCzyAutomatyzujacy() { return czyAutomatyzujacy; }
+    public int getLiczbaScenariuszy()    { return liczbaScenariuszy; }
+
+    // Tester automatyzujący dostaje bonus 15%, manualny – stawkę bazową
+    @Override
+    public double obliczKosztMiesieczny() {
+        return czyAutomatyzujacy ? getStawkaBazowa() * 1.15 : getStawkaBazowa();
+    }
+
+    @Override
+    public String przedstawSie() {
+        String typ = czyAutomatyzujacy ? "Automatyzujący" : "Manualny";
+        return "Tester (" + typ + "): " + getImie() + " " + getNazwisko()
+                + " | Scenariusze: " + liczbaScenariuszy
+                + " | Koszt: " + String.format("%.2f", obliczKosztMiesieczny()) + " PLN";
+    }
+
+    // Metoda specyficzna dla Testera
+    public void uruchomRaportTestow() {
+        System.out.println(">>> Raport testów - " + getImie() + " " + getNazwisko() + ":");
+        System.out.println("    Typ testowania: " + (czyAutomatyzujacy ? "Automatyzacja" : "Manualne"));
+        System.out.println("    Liczba scenariuszy testowych: " + liczbaScenariuszy);
+        System.out.println("    Status: Gotowy do wdrożenia");
+    }
+
+    @Override
+    public String toString() {
+        return "Tester{id='" + pobierzIdPracownika() + "', imie='" + getImie()
+                + "', nazwisko='" + getNazwisko()
+                + "', automatyzujacy=" + czyAutomatyzujacy
+                + ", scenariusze=" + liczbaScenariuszy
+                + ", kosztMiesieczny=" + String.format("%.2f", obliczKosztMiesieczny()) + " PLN}";
+    }
 }
